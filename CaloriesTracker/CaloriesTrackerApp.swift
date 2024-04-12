6727cb5c174dcdd417455ce0f9c63176d13119b8
Min Pyae <minmp2002@gmail.com>
2024-04-12 12:34:51 +0100

    Initial Commit

---
+++ CaloriesTracker/CaloriesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesTrackerApp.swift
+//  CaloriesTracker
+//
+//  Created by Chris Min on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
